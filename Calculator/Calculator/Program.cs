29f228040eb379d7a9e41f6d88b56ee0b74e6675
OgorelinR <dden85@mail.ru>
2021-09-29 17:19:37 +0300

    first commit

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                int x = GetNumber("Пожалуйста, введите первое число");
+                int y = GetNumber("Пожалуйста, введите второе число");
+                Console.WriteLine("Пожалуйста, выберите команду: +, -, *, /, min, max");
+                String cmd = Console.ReadLine();
+                Console.WriteLine(GetResult(x, y, cmd));
+                Console.WriteLine("Хотите ли вы продолжить? да/нет");
+                string cont = Console.ReadLine();
+                if (cont.ToLower().Contains("нет"))
+                    break;
+            } while (true);
+        }
+
+        static int GetMax(int a, int b) {
+            int max = 0;
+            if (a > b)
+                max = a;
+            else
+                max = b;
+            return max;
+        }
+        static int GetMin(int a, int b)
+        {
+            int min = 0;
+            if (a > b)
+                min = b;
+            else
+                min = a;
+            return min;
+        }
+        static int GetNumber(string text) {
+            Console.WriteLine(text);
+            while (true) {
+                string str = Console.ReadLine();
+                int result;
+                if (Int32.TryParse(str, out result))
+                    return result;
+                else
+                    Console.WriteLine("Неправильный формат числа. Попробуйте ещё раз.");
+            }
+        }
+        private static double GetResult(int x, int y, string cmd) {
+            double result;
+            switch (cmd) {
+                case "+": 
+                    result = x + y;
+                    break;
+                case "-": 
+                    result = x - y;
+                    break;
+                case "*": 
+                    result = x * y;
+                    break;
+                case "/":
+                    result = (double) x / y;
+                    break;
+                case "min":
+                    result = GetMin(x, y);
+                    break;
+                case "max":
+                    result = GetMax(x, y);
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+            return result;
+        }
+    }
+}
---
+++ Скобки/Скобки/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Скобки
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите строку для определения корректной посследовательности скобок:");
+            do
+            {
+                string str_input = Console.ReadLine();
+                string str_CheckOut = ThatStringIsEmpty(str_input);
+
+
+                if (str_CheckOut.IndexOf("Начинаю") >= 0)
+                {
+                    bool bool_result = CheckAll(str_input);
+                    if (bool_result)
+                    {
+                        Console.WriteLine ("ШЕФ! Всё совпало!\nХотите повторить снова? y/n");
+                    }
+                    else
+                    {
+                        Console.WriteLine ("Несоответствие скобок!\nХотите повторить снова? y/n");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(str_CheckOut);
+                }
+
+            }
+            while (Console.ReadKey(true).Key == ConsoleKey.Y);
+        }
+
+
+        static bool CheckAll (string str_inputString)
+        {
+            Stack<char> stack_Chars = new Stack<char>();
+
+            // Цикл проверка каждого символа во входящей строке
+            foreach (char c in str_inputString)
+            {
+                switch(c)
+                {
+                    //Определяем есть ли открытая скобка
+                    case '(':
+                    case '[':
+                    case '{':
+                        // Если есть открытая - закидываем в стэк
+                        stack_Chars.Push(c);
+                        break;
+
+                    case ')':
+                        // Если стэк пустой или у нас нет открытой скобки, то False
+                        if (stack_Chars.Count > 0 && stack_Chars.Peek() == '(')
+                        {
+                            stack_Chars.Pop();  //Удаляем из стэка элемент
+                            break;
+                        }
+                        return false;
+
+
+                    case ']':
+                        // Если стэк пустой или у нас нет открытой скобки, то False
+                        if (stack_Chars.Count > 0 || stack_Chars.Peek() == '[')
+                        {
+                            stack_Chars.Pop();  //Удаляем из стэка элемент
+                            break;
+                        }
+                        return false;
+
+
+                    case '}':
+                        // Если стэк пустой или у нас нет открытой скобки, то False
+                        if (stack_Chars.Count > 0 || stack_Chars.Peek() == '{')
+                        {
+                            stack_Chars.Pop();  //Удаляем из стэка элемент
+                            break;
+                        }
+                        return false;
+                    default:
+                        break;
+                }
+            }
+            //Возвращаем TRUE, если после обработки кол-во элементов в стэке = 0
+            return stack_Chars.Count == 0;
+        }
+
+        static string ThatStringIsEmpty (string str_inputString)
+        {
+
+            if (str_inputString.Length <= 0) 
+            {
+                return "Вы ввели пустую строку!\nХотите повторить снова? y/n";
+            }
+            else
+        
+                if(str_inputString.IndexOf("(") >= 0 || str_inputString.IndexOf("[") >= 0  || str_inputString.IndexOf("{") >= 0 || str_inputString.IndexOf(")") >= 0 || str_inputString.IndexOf("]") >= 0 || str_inputString.IndexOf("}") >= 0)
+                {
+                    return "Начинаю обработку\n";
+
+                }
+                else
+                {
+                    return "Ни одной скобки не найдено! \nХотите повторить снова? y/n";
+                }
+            }
+        }
+
+    }
+
